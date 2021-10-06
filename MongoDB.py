c392fb4d1100e90faea9b002eb59054afeac033e
aareva-2 <78007494+aareva-2@users.noreply.github.com>
2021-10-06 09:55:59 -0400

    Mock Data Setup

---
+++ MongoDB.py
@@ -0,0 +1,44 @@
+from pymongo import MongoClient
+import certifi
+
+
+def main():
+
+    ca = certifi.where()
+    client = MongoClient('mongodb+srv://aareva2:Lincoln1233@cluster0.gudfj.mongodb.net/ResearchU?'
+                         'retryWrites=true&w=majority', tlsCAFile=ca) # MongoClient requires TLS certificate and
+                                                                    # sometimes python is unable to request the TLS
+                                                                      # so specify to use certifi package
+    db = client.ResearchU
+    # research = db.research
+    # researchOpportunity = {
+    #     'name': 'Dr. Fossati',
+    #     'description': 'A Test research opportunity'
+    # }
+    # research.insert_one(researchOpportunity)
+
+    # profile = db.studentProfile
+    # student = {
+    #     'name': 'Abraham Arevalo',
+    #     'Major': 'Computer Science'
+    # }
+    # profile.insert_one(student)
+    # profile = db.professorProfile
+    # professor = {
+    #     'Name': 'Dr. Fossati',
+    #     'School': 'Emory University',
+    #     'Research' : True
+    # }
+    # profile.insert_one(professor)
+
+    # adminprofile = db.adminProfile
+    # admin = {
+    #     'Name': 'Jane Doe',
+    #     'Admin Status': True
+    # }
+    # adminprofile.insert_one(admin)
+    # print(client.ResearchU.research.find_one({'name': 'Abraham Arevalo'}))
+
+
+if __name__ == "__main__":
+    main()
