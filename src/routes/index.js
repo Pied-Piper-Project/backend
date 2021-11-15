66e32dcebcebf80fe516dbb427e838a470caacb9
caroltang0331 <69888386+caroltang0331@users.noreply.github.com>
2021-11-14 20:54:59 -0500

    -update index.js with verifyEmailRoute, i forgot last time

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { signInRoute } from "./signInRoute"
 import { signUpRoute } from "./signUpRoute";
 import { testRoute } from "./testRoute";
 import { updateUserInfoRoute} from "./updateUserInfoRoute";
-import { CreatePostsRoute } from "./CreatePostsRoute"; 
+import { CreatePostsRoute } from "./CreatePostsRoute";
+import { verifyEmailRoute} from "./verifyEmailRoute";
 
 export const routes = [
     signInRoute,
     signUpRoute,
     testRoute,
+    verifyEmailRoute,
     updateUserInfoRoute,
     CreatePostsRoute
 ];
