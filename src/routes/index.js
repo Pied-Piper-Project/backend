6cd0ee8e9fee9a210ad20a61b327b02b9a40f1e6
Schm2000 <ender.l.schmidt@emory.edu>
2021-11-21 20:16:31 -0500

    PIED-110
    To be used in conjunction with the frontend's PIED-110.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { testRoute } from "./testRoute";
 import { updateUserInfoRoute} from "./updateUserInfoRoute";
 import { CreatePostsRoute } from "./CreatePostsRoute";
 import { verifyEmailRoute} from "./verifyEmailRoute";
+import { applyForPostRoute } from "./applyForPostRoute";
 
 export const routes = [
     signInRoute,
@@ -11,5 +12,6 @@ export const routes = [
     testRoute,
     verifyEmailRoute,
     updateUserInfoRoute,
-    CreatePostsRoute
+    CreatePostsRoute,
+    applyForPostRoute
 ];
