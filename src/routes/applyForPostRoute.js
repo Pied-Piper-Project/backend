959ef4f35e77174ba3445c6b7e393e2e4289e365
Schm2000 <ender.l.schmidt@emory.edu>
2021-11-30 21:04:01 -0500

    Fixes
    The backend for this route should be completely fixed and ready to push now.

--- src/routes/applyForPostRoute.js
+++ src/routes/applyForPostRoute.js
@@ -1,30 +1,40 @@
 import { getDbConnection } from "../db";
 import { ObjectID } from "bson";
+import jwt from 'jsonwebtoken';
 
 export const applyForPostRoute = {
     path: '/api/ResearchU/apply/:postId',
     method: 'put',
     handler: async (req, res) => {
-        const{id} = req.params;
+        const{allId} = req.params;
+        const {authorization} = req.headers;
         const db = getDbConnection('ResearchU');
         const studentID = req.body.studentID
         const appliedPostsU = req.body.appliedPosts
         const applicantsU = req.body.applicants
         const postID = req.body.postID
 
-        console.log("Here!")
-        console.log(studentID)
-        console.log(appliedPostsU)
-        console.log(applicantsU)
-        console.log(postID)
+        const token = authorization.split(' ')[1];
 
-        const resultOne = await db.collection('studentProfile').updateOne(
-            {_id: ObjectID(studentID)}, {$push: {appliedPosts: appliedPostsU}}
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+
+        const resultOne = await db.collection('studentProfile').findOneAndUpdate(
+            {_id: ObjectID(studentID)}, {$push: {appliedPosts: appliedPostsU}}, { returnOriginal: false}
         );
-        console.log("Got here!")
+        const {email, isVerified, appliedPosts, info} = resultOne.value;
+        appliedPosts.push(postID)
+        const id = studentID
         const resultTwo = await db.collection('research').updateOne(
             {_id: ObjectID(postID)}, {$push: {applicants: applicantsU}}
         );
-        res.sendStatus(200);
+        jwt.sign({id, email, isVerified, appliedPosts, info}, process.env.JWT_SECRET, {expiresIn: '2d'}, (err, token) => {
+            if (err) {
+                return res.status(200).json(err);
+            }
+            res.status(200).json({token});
+        });
+        //console.log("Why")
+        //res.sendStatus(200);
+     })
     }
 }
