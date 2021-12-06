c251a7930183e2a64fbb3c6b94d6f9dbaf69be04
caroltang0331 <69888386+caroltang0331@users.noreply.github.com>
2021-12-05 23:44:30 -0500

    -finalize sign in: allow specified accout sign in with same email

--- src/routes/signInRoute.js
+++ src/routes/signInRoute.js
@@ -6,17 +6,28 @@ export const signInRoute = {
     path: '/api/ResearchU/SignIn',
     method: 'post',
     handler: async (req, res) => {
-        const {email, password } = req.body;
+        const {email, password, signin } = req.body;
+        let user;
 
         const db = getDbConnection('ResearchU');
-        const user = await db.collection('studentProfile').findOne({email});
+        if (signin === "Student") {
+            user = await db.collection('studentProfile').findOne({email});
+        }
+        else if (signin === "Professor") {
+            user = await db.collection('professorProfile').findOne({email});
+        }
+        else {
+            user = await db.collection('adminProfile').findOne({email});
+        }
+        //const user = await db.collection('studentProfile').findOne({email});
 
         if (!user) return res.sendStatus(401);
 
-        const { _id: id, isVerified, passwordHash, appliedPosts, info} = user;
-        const isCorrect = await bcrypt.compare(password, passwordHash);
-        if (isCorrect) {
-            jwt.sign({ id, isVerified, email, appliedPosts, info},
+        if (signin === "Student") {
+            const { _id: id, isVerified, passwordHash, appliedPosts, info} = user;
+            const isCorrect = await bcrypt.compare(password, passwordHash);
+            if (isCorrect) {
+                jwt.sign({ id, isVerified, email, appliedPosts, info},
                             process.env.JWT_SECRET, {expiresIn: '2d'},
                     (err, token) => {
                                 if (err) {
@@ -24,9 +35,60 @@ export const signInRoute = {
                                 }
                                 res.status(200).json({token});
                              }
-            );
-        } else {
-            res.sendStatus(401);
+                );
+            } else {
+                res.sendStatus(401);
+            }
         }
+        else if (signin === "Professor") {
+            const { _id: id, isVerified, passwordHash, createdPosts, info} = user;
+            const isCorrect = await bcrypt.compare(password, passwordHash);
+            if (isCorrect) {
+                jwt.sign({ id, isVerified, email, createdPosts, info},
+                            process.env.JWT_SECRET, {expiresIn: '2d'},
+                    (err, token) => {
+                                if (err) {
+                                    res.status(500).json(err);
+                                }
+                                res.status(200).json({token});
+                             }
+                );
+            } else {
+                res.sendStatus(401);
+            }
+        }
+        else {
+            const { _id: id, isVerified, passwordHash, createdPosts, info} = user;
+            const isCorrect = await bcrypt.compare(password, passwordHash);
+            if (isCorrect) {
+                jwt.sign({ id, isVerified, email, createdPosts, info},
+                            process.env.JWT_SECRET, {expiresIn: '2d'},
+                    (err, token) => {
+                                if (err) {
+                                    res.status(500).json(err);
+                                }
+                                res.status(200).json({token});
+                             }
+                );
+            } else {
+                res.sendStatus(401);
+            }
+        }
+
+        //const { _id: id, isVerified, passwordHash, appliedPosts, info} = user;
+        // const isCorrect = await bcrypt.compare(password, passwordHash);
+        // if (isCorrect) {
+        //     jwt.sign({ id, isVerified, email, appliedPosts, info},
+        //                     process.env.JWT_SECRET, {expiresIn: '2d'},
+        //             (err, token) => {
+        //                         if (err) {
+        //                             res.status(500).json(err);
+        //                         }
+        //                         res.status(200).json({token});
+        //                      }
+        //     );
+        // } else {
+        //     res.sendStatus(401);
+        // }
     }
 }
